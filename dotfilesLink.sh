a2cb9a0ed80a253539c8c452767b234c45be1475
tanopanta <rainstorm2525@gmail.com>
2020-08-17 23:17:16 +0900

    add apt install

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,8 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+# install
+
+sudo apt install tmux zsh 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
